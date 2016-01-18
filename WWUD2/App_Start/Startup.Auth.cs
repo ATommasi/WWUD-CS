bad601e9b65335c3d41384fc393395b4d36b6621
ArcaneChaos <atommasi@gmail.com>
2016-01-18 13:31:49 -0500

    namcing changes

--- WWUD2/App_Start/Startup.Auth.cs
+++ WWUD2/App_Start/Startup.Auth.cs
@@ -5,9 +5,9 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
-using WWUD2.Models;
+using WUDIF.Models;
 
-namespace WWUD2
+namespace WUDIF
 {
     public partial class Startup
     {
